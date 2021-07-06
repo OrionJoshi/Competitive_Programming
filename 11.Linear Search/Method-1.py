d38fd340e45e994f74dd317ea8ab01230607c04e
Khageshwor <khageshwarjoshi03@gmail.com>
2021-07-06 09:38:12 +0545

    Linear search solution added

---
+++ 11.Linear Search/Method-1.py
@@ -0,0 +1,20 @@
+# Method-1 Iterative Method
+
+def linearSearch(arr, elementToSearch):
+  for x in arr:
+    if x == elementToSearch:
+      return arr.index(x)
+  return -1
+
+
+numArray = [10,2,3,44,5]
+key = 10
+
+result = linearSearch(numArray, key)
+
+if(result != -1):
+  print(f'Element Found at index {result}')
+else:
+  print("Element Not Found!!")
+
+# Time Complexity : O(n)
