9a578405af2df344a328fa0bfed62a8ba254ec05
khageshwor <khageshwarjoshi03@gmail.com>
2022-01-09 18:05:27 +0545

    Add: single cycle check solution

---
+++ 198.Single Cycle Check/Method-1.py
@@ -0,0 +1,19 @@
+# Method-1
+
+def hasSingleCycle(array):
+    numElementsVisited = 0
+    currentIdx = 0
+    while numElementsVisited < len(array):
+        if numElementsVisited > 0 and currentIdx == 0:
+            return False
+        numElementsVisited += 1
+        currentIdx = getNextIdx(currentIdx, array)
+    return currentIdx == 0
+    
+def getNextIdx(currentIdx, array):
+    jump = array[currentIdx]
+    nextIdx = (currentIdx + jump) % len(array)
+    return nextIdx if nextIdx >= 0 else nextIdx + len(array)
+
+# Time Complexity : O(n)
+# Space Complexity : O(1)
