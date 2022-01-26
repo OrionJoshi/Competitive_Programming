8ae05c9d061fd26594306981f24d7179ce5921d3
khageshwor <khageshwarjoshi03@gmail.com>
2022-01-26 16:51:21 +0545

    Add: find pivot index solution

---
+++ 215.Find Pivot Index/Method-1.py
@@ -0,0 +1,16 @@
+# Method-1
+
+def pivotIndex(nums):
+    leftSum = 0
+    rightSum = sum(nums)
+
+    for i in range(len(nums)):
+        rightSum  -= nums[i]
+        if leftSum == rightSum:
+            return i
+        leftSum += nums[i]
+
+    return -1
+
+# Time Complexity : O(n)
+# Space Complexity : O(1)
