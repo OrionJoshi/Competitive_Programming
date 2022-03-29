8317fa3637e60a96a4e14df0d9c100f8ae28de9a
khageshwor <khageshwarjoshi03@gmail.com>
2022-03-29 19:49:36 +0545

    Add: Intersection of Two Linked Lists solution

---
+++ 275.Intersection of Two Linked Lists/Method-1.py
@@ -0,0 +1,12 @@
+# Method-1
+
+def getIntersectionNode(headA, headB):
+    h1 = headA
+    h2 = headB
+    while h1 != h2:
+        h1 = h1.next if h1 else headB
+        h2 = h2.next if h2 else headA
+    return h1
+
+# Time Complexity : O(n)
+# Space Complexity : O(n)
