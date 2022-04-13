9988bffa8379321a351fe4c57dfacdba75b54578
khageshwor <khageshwarjoshi03@gmail.com>
2022-04-13 22:06:55 +0545

    Add: Number of Different Integers in a String solution

---
+++ 288.Number of Different Integers in a String/Method-1.py
@@ -0,0 +1,12 @@
+# Method-1
+
+import re
+
+def numDifferentIntegers(word):
+    word = re.findall('(\d+)', word)
+    numbers = [int(i) for i in word]
+    
+    return len(set(numbers))
+
+# Time Complexity : O(n)
+# Space Complexity : O(n)
