a9d44afa3a871b2c60515536282d6f64e4f4842a
khageshwor <khageshwarjoshi03@gmail.com>
2022-07-15 21:01:39 +0545

    Add: Winner of an Array Game solution

---
+++ 347.Winner of an Array Game/Method-1.py
@@ -0,0 +1,20 @@
+# Method-1
+
+def getWinner(arr, k):
+    if k == 1:
+        return max(arr[0], arr[1])
+    i = wins = 0
+    
+    for j in range(1, len(arr)):
+        if arr[i] > arr[j]:
+            wins += 1
+            if wins == k:
+                return arr[i]
+        else:
+            i = j
+            wins = 1
+    
+    return max(arr)
+
+# Time Complexity : O(n)
+# Space Complexity : O(1)
